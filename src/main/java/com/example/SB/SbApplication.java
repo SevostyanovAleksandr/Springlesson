a1b608568a49440a9db9c47a1d022b4e114a2c6e
sevos <sevoalex@mail.ru>
2021-10-17 17:01:28 +0300

    учебный проект

---
+++ src/main/java/com/example/SB/SbApplication.java
@@ -0,0 +1,13 @@
+package com.example.SB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbApplication.class, args);
+	}
+
+}
